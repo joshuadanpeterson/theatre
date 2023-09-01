1009f8fa55abb2157ed3500ac8a99b9e63c47d18
Josh Peterson <joshuadanpeterson@gmail.com>
2023-09-01 03:05:53 -0600

    theatre exercise

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const GENERAL_ADMISSION_TICKET_COST = 20;
+const CHILD_AND_SENIOR_TICKET_COST = 10;
+const MATINEE_DISCOUNT = 3;
+
+function buyTicket() {
+    const age = prompt('What is your age?');
+    let cost = getBaseTicketCost(age);
+    const isMatinee = prompt('Are you attending a matinee show?').toLowerCase();
+
+    if ((isMatinee === 'yes' || isMatinee === 'Yes' || isMatinee === 'y') && age > 12 && age < 65) {
+        cost -= MATINEE_DISCOUNT;
+    }
+
+    if (age <= 12 || age >= 65) {
+        cost = CHILD_AND_SENIOR_TICKET_COST;
+    } alert('Your ticket will cost: $' + cost);
+}
+
+function getBaseTicketCost(age) {
+    if (age <= 12 || age >= 65) {
+        return CHILD_AND_SENIOR_TICKET_COST;
+    } return GENERAL_ADMISSION_TICKET_COST;
+}
